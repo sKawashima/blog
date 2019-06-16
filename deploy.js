d0829193fcbf79fbf333eb83783c87ab0edcb515
sKawashima <bysk.333@gmail.com>
2019-06-16 11:43:35 +0900

    いろいろとうまくいかない

--- deploy.js
+++ deploy.js
@@ -1,30 +1,41 @@
-'use struct'
-
 const fs = require('fs')
 const Ftp = require('ftp')
 const ftp = new Ftp()
+const jschardet = require('jschardet')
+const Iconv = require('iconv').Iconv
 
-const serverPath = '/skawashima.com/blog/' // 接続するサーバー内Pathを定義
+const serverPath = '/skawashima.com/test/' // 接続するサーバー内Pathを定義
 
 const removeExistingFile = (err, list) => { // リストのファイルを全て削除
   if (err) throw err
-  for (const file of list) {
-    if (file.type !== 'd') {
-      ftp.delete(`${serverPath}${file.name}`, (err) => {
-        if (err) throw err
-        console.log(`deleted: ${file.name}`)
-        ftp.end()
-      })
-    }
-  }
+  // for (const file of list) {
+  //   if (file.type !== 'd') {
+  //     console.log(`deleting${file.name}\n\n`)
+  //     const detectEncoding = jschardet.detect(file.name).encoding
+  //     console.log(detectEncoding)
+  //     const iconv = new Iconv(detectEncoding, 'ASCII//TRANSLIT//IGNORE')
+
+  //     const covertedFileName = iconv.convert(file.name).toString()
+  //     console.log(covertedFileName)
+
+  //     ftp.delete(`${serverPath}${file.name}`, (err) => {
+  //       if (err) throw err
+  //       console.log(`deleted: ${file.name}`)
+  //       ftp.end()
+  //     })
+  //   }
+  // }
+  ftp.delete('skawashima.com/test', err => {
+    console.error(err)
+    
+  })
 }
 
 const upload = () => {
-  const files = fs.readdirSync('./dist')
-  console.log(files)
+  const files = fileRead('dist')
   for (const file of files) {
-    console.log(file)
-    ftp.put(`./dist/${file}`, `${serverPath}${file}`, (err) => {
+    console.log(`uploding: ${file}`)
+    ftp.put(`${file}`, `${serverPath}${file}`, (err) => {
       if (err) throw err
       console.log(`uploaded: ${file}`)
       ftp.end()
@@ -34,7 +45,8 @@ const upload = () => {
 
 ftp.on('ready', () => { // ↓のconnectが成功したら発火
   ftp.list(serverPath, (err, list) => { // 既存ファイルのリストを取得
-    // removeExistingFile(err, list) // 既存ファイルの削除
+    console.log(list)
+    removeExistingFile(err, list) // 既存ファイルの削除
     upload() // ファイルをアップロード
   })
 })
@@ -44,3 +56,23 @@ ftp.connect({ // FTP接続する
   user: 'sd0979660@gmoserver.jp',
   password: '6$MGr6EmdQCd5kyY'
 })
+
+const fileRead = (path) => {
+  const fileList = []
+  files = fs.readdirSync(path, { withFileTypes: true })
+  files.forEach((file) => {
+    if (file.isDirectory()) {
+      dir_files = fileRead(`${path}/${file.name}`)
+      dir_files.forEach((dir_file) => {
+        fileList.push(dir_file)
+      })
+    } else {
+      if (file.name !== '.DS_Store') {
+          fileList.push(`${path}/${file.name}`.replace('dist/', ''))
+      }
+    }
+  })
+  return fileList
+}
+
+// console.log(fileRead('./dist'))
